ce9afb96a6a4547fd07daae9d9a757edfe6871a8
丁昱清 <9119799+ding-yuqing@user.noreply.gitee.com>
2023-02-22 08:10:03 +0000

    update examples/PEO-Ga+LLZO/samples_to_generate.py.
    Signed-off-by: 丁昱清 <9119799+ding-yuqing@user.noreply.gitee.com>

--- examples/PEO-Ga+LLZO/samples_to_generate.py
+++ examples/PEO-Ga+LLZO/samples_to_generate.py
@@ -1,8 +1,8 @@
 import numpy as np
 import datetime
 import random
-from initial_structure_new import initial_stru
-from ions_jumping_process_new1 import ions_jump
+from initial_structure import initial_stru
+from ions_jumping_process import ions_jump
 
 def sample_generation(jump_num,rate_hopping_a, rate_hopping_b,rate_hopping_c,coor_doped_label,ions_logic_coor,initial_cell_coor1,NX,NY,NZ):
 
