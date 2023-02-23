73b31ab49b686fd8574d99eb194bb5135e006c06
丁昱清 <9119799+ding-yuqing@user.noreply.gitee.com>
2023-02-23 14:17:11 +0000

    update erpipc/file_hand.py.
    Signed-off-by: 丁昱清 <9119799+ding-yuqing@user.noreply.gitee.com>

--- erpipc/file_hand.py
+++ erpipc/file_hand.py
@@ -12,56 +12,22 @@ import os
 import copy
 from multiprocessing import Process
 
-def readfile(path):
-    dataframe = pd.read_csv(path, header=None, delimiter="/0", engine='python')  # 文件地址
-    initial_information = dataframe
-    # print (initial_information[5][2])
-    return initial_information
-
 
 
 def mkdir(path):
-    # 去除首位空格
+    '''create file'''
     path = path.strip()
-    # 去除尾部 \ 符号
     path = path.rstrip("\\")
-    # path = path.rstrip("//")
-    # 判断路径是否存在
-    # 存在     True
-    # 不存在   False
+
+    # Determine whether the path exists, Existence true, not exist False
     isExists = os.path.exists(path)
-    # 判断结果
+    # If not, create a directory
     if not isExists:
-        # 如果不存在则创建目录
-        # 创建目录操作函数
         os.makedirs(path)
         print(path + ' 创建成功')
         return True
     else:
-        # 如果目录存在则不创建，并提示目录已存在
+        # If the directory exists, it is not created and prompts that the directory already exists
         print(path + ' 目录已存在')
-        return False  # 读取文件内容并打印
-
-def save_sample(samp, path):
-    print('saving simple')
-    filePath = os.getcwd()
-    dictionay = filePath + path + "//"
-    mkdir(dictionay)
-    for i in range(0, len(samp)):
-        t = dictionay + "sample" + str(i)
-        data_list = samp[i]
-        # convert list to array
-        # data_array = np.array(data_list)
-        # print(type(data_array))
-        # saving...
-        f = open(t, 'w')
-        for k in data_list:
-            for j in k:
-                f.write(str(j))
-                f.write(',')
-            f.write('\n')
-        f.close()
-        # np.savetxt(t, data_array, delimiter=',')
-        if i % (len(samp) / 10) == 0:
-            print(i / len(samp) * 100, "%", end="")
+        return False  
 
